721bffcc0de31dfe0904f8d938d3fe3e2ad228cc
Shamil Ibrahim <shamilibrahimk@gmail.com>
2018-07-14 18:51:40 +0530

    Create madlibs-generator.py

---
+++ madlibs-generator.py
@@ -0,0 +1,12 @@
+#prompts for a series of input
+programmer = input("Someones Name: ")
+company = input("A Company Name: ")
+language1 = input("A Programming Language: ")
+recruiter = input("Someone else's Name: ")
+language2 = input("Another Programmin Language: ")
+
+#printing the story with the given inputs
+print("--------------------------------------------------------------------")
+print(programmer + " went to attend an interview at " + company + " for the first time.")
+print("He knows well to code in " + language1 +".")
+print(recruiter + ', the HR says that "we need a ' + language2 + ' developer, so You can go home".')
