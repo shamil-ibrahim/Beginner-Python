db2ccd1879321ef25a8a14eefedd94bdb2f67731
Shamil Ibrahim <shamilibrahimk@gmail.com>
2018-07-19 20:16:13 +0530

    Create team-chooser.py

---
+++ team-chooser.py
@@ -0,0 +1,22 @@
+from random import choice
+
+players = ['Sam', 'John', 'Mark', 'Elon', 'Joy', 'Tim', 'Rony', 'Chan']
+teams = ['Python', 'Java', 'Ruby', 'Javascript', 'Hadoop', 'Kotlin']
+
+print('\nPlayers: ', players)
+print('Teams: ', teams)
+
+teamA = []
+teamB = []
+
+while len(players) > 0:
+	playerA = choice(players)
+	teamA.append(playerA)
+	players.remove(playerA)
+
+	playerB = choice(players)
+	teamB.append(playerB)
+	players.remove(playerB)
+
+print('\nTeam A: ', teamA)
+print('Team B: ', teamB)
